74951f744a52ebc437f6973501fe23d59f8ebd40
Pawel Pohl <pawel.pohl229@gmail.com>
2018-05-16 22:13:41 +0200

    DBConnection singleton implementation.

--- src/main/java/motorhomes/com/examproject/util/DbConnection.java
+++ src/main/java/motorhomes/com/examproject/util/DbConnection.java
@@ -8,11 +8,15 @@ public class DbConnection {
         private final static String USERNAME = "motorhomes4";
         private final static String PASSWORD = "#project";
         private final static String CONNSTRING = "jdbc:mysql://den1.mysql4.gear.host/motorhomes4?useSSL=false";
+        private static Connection connection;
 
         public static Connection getConnection() {
 
             try {
-                return DriverManager.getConnection(CONNSTRING, USERNAME, PASSWORD);
+                if (connection == null){
+                    connection = DriverManager.getConnection(CONNSTRING, USERNAME, PASSWORD);
+                }
+                return connection;
 
             } catch (SQLException e) {
                 e.printStackTrace();
