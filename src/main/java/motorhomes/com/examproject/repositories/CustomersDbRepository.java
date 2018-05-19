c8c53e20b32d45a1922dbab32e1510a75585d991
Pawel Pohl <pawel.pohl229@gmail.com>
2018-05-19 12:26:44 +0200

    CustomerDBRepository names standardization.

--- src/main/java/motorhomes/com/examproject/repositories/CustomersDbRepository.java
+++ src/main/java/motorhomes/com/examproject/repositories/CustomersDbRepository.java
@@ -14,70 +14,70 @@ import java.util.ArrayList;
  */
 public class CustomersDbRepository implements ICrudRepository<Customer> {
 
-    private Connection conn;
-    private PreparedStatement preSt;
-    private ResultSet res;
+    private Connection connection;
+    private PreparedStatement statement;
+    private ResultSet resultSet;
 
     public CustomersDbRepository() throws SQLException{
-        this.conn = DbConnection.getConnection();
+        this.connection = DbConnection.getConnection();
     }
 
     @Override
     public ArrayList<Customer> readAll() throws SQLException {
         ArrayList<Customer> customers = new ArrayList<>();
-        preSt = conn.prepareStatement("SELECT * FROM customer");
-        res = preSt.executeQuery();
+        statement = connection.prepareStatement("SELECT * FROM customers");
+        resultSet = statement.executeQuery();
 
-        while (res.next()){
-            customers.add(new Customer(res.getInt("customer_id"), res.getString("customers_name"), res.getString("driving_licence_nr")));
+        while (resultSet.next()){
+            customers.add(new Customer(resultSet.getInt("customer_id"), resultSet.getString("customers_name"), resultSet.getString("driving_licence_nr")));
         }
-        preSt = null;
-        res = null;
+        statement = null;
+        resultSet = null;
         return customers;
     }
 
     @Override
     public boolean create(Customer customer) throws SQLException {
         System.out.println(customer);
-        preSt = conn.prepareStatement("INSERT INTO customers(customers_name, driving_licence_nr) VALUES (?,?)");
-        preSt.setString(1, customer.getCustomerName());
-        preSt.setString(2, customer.getDrivingLicenseNr());
-        boolean creationSuccessful = preSt.execute();
-        preSt = null;
+        statement = connection.prepareStatement("INSERT INTO customers(customers_name, driving_licence_nr) VALUES (?,?)");
+        statement.setString(1, customer.getCustomerName());
+        statement.setString(2, customer.getDrivingLicenseNr());
+        boolean creationSuccessful = statement.execute();
+        statement = null;
         return creationSuccessful;
     }
 
     @Override
     public Customer read(int customerId) throws SQLException {
-        preSt = conn.prepareStatement("SELECT * FROM customers WHERE customer_id = ?");
-        preSt.setInt(1, customerId);
-        res = preSt.executeQuery();
+        statement = connection.prepareStatement("SELECT * FROM customers WHERE customer_id = ?");
+        statement.setInt(1, customerId);
+        resultSet = statement.executeQuery();
         Customer customer = null;
 
-        if (res.next()){
-            customer = new Customer(res.getInt("customer_id"), res.getString("customers_name"), res.getString("driving_licence_nr"));
+        if (resultSet.next()){
+            customer = new Customer(resultSet.getInt("customer_id"), resultSet.getString("customers_name"), resultSet.getString("driving_licence_nr"));
         }
-        preSt = null;
-        res = null;
+        statement = null;
+        resultSet = null;
         return customer;
     }
 
     @Override
     public void update(Customer customer) throws SQLException {
 
-        preSt = conn.prepareStatement("UPDATE customers SET customers_name=?, driving_licence_nr=? WHERE customer_id=?");
-        preSt.setString(1, customer.getCustomerName());
-        preSt.setString(2, customer.getDrivingLicenseNr());
-        preSt.setInt(3, customer.getCustomerId());
-        preSt.execute();
-        preSt = null;
+        statement = connection.prepareStatement("UPDATE customers SET customers_name=?, driving_licence_nr=? WHERE customer_id=?");
+        statement.setString(1, customer.getCustomerName());
+        statement.setString(2, customer.getDrivingLicenseNr());
+        statement.setInt(3, customer.getCustomerId());
+        statement.execute();
+        statement = null;
     }
 
     @Override
     public void delete(int customerId) throws SQLException {
-        preSt = conn.prepareStatement("DELETE FROM customers WHERE customer_id=?");
-        preSt.setInt(1, customerId);
-        preSt.execute();
-        preSt = null;
+        statement = connection.prepareStatement("DELETE FROM customers WHERE customer_id=?");
+        statement.setInt(1, customerId);
+        statement.execute();
+        statement = null;
     }
 }
