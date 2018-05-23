612a6abd87df50ca8b5aabab4bc7b9c97513cff5
Alicja Drankowska <alicja14@gmail.com>
2018-05-23 02:11:27 +0200

    MotorhomeManager changed, new html files (about motorhomes) created, get/post mapping methods added to MotorhomeController.

--- src/main/java/motorhomes/com/examproject/applicationLogic/MotorhomeManager.java
+++ src/main/java/motorhomes/com/examproject/applicationLogic/MotorhomeManager.java
@@ -52,11 +52,20 @@ public class MotorhomeManager {
        return motorhome;
     }
 
-    public boolean saveMotorhomeDescription(Motorhome motorhome, MotorhomeDescription motorhomeDescription){
+    public boolean saveMotorhomeDescription(Motorhome motorhome, int motorhomeDescriptionId){
+        try {
+            motorhome.setMotorhomeDescription(motorhomeDescriptionDbRepository.read(motorhomeDescriptionId));
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    /**public boolean saveMotorhomeDescription(Motorhome motorhome, MotorhomeDescription motorhomeDescription){
         try{
             /*Nie wiem jak to zaplanowalas ale czy motorhomeDescription.getMotorhomeDescriptionId() nie zwraca czasem bledu ze nie zostal zinicjalizowany jesli towrzysz nowe description?
            Jesli tak to napisz i ci powiem jak temu zapobiec
-            */
+
 
             MotorhomeDescription savedMotorhomeDescription = motorhomeDescriptionDbRepository.read(motorhomeDescription.getMotorhomeDescriptionId());
             if (savedMotorhomeDescription == null) {
@@ -64,6 +73,15 @@ public class MotorhomeManager {
                 savedMotorhomeDescription = motorhomeDescriptionDbRepository.read(motorhomeDescription.getMotorhomeDescriptionId());
             }
             motorhome.setMotorhomeDescription(savedMotorhomeDescription);
+            motorhomeDbRepository.create(motorhome);
+        }catch (SQLException e){
+            e.printStackTrace();
+            return false;
+
+        }
+        return true;
+    }*/
+
             // W tym miejscu ja zwalilem sprawe, jesli pojdziesz do MotorhomeDBRepository to statement.execute() nie zwraca
             // true w momencie w ktorym polecenie zostalo wykonane, tylko w momencie w ktorym zapytanie do bazy danych zwraca jakis wynik(ResultSet).
             // Czyli kiedy towrzysz nowy wiersz to ta funckja bedzie zwraca false, bo zapytanie "INSERT INTO" nie zwraca zadnego wyniku.
@@ -81,21 +99,24 @@ public class MotorhomeManager {
             // a jesli wszystko pojdzie zgodnie z planem i ta funckja dojdzie do konca to zwracasz true
             // To samo tyczy sie innych funkcji z baz danych ktore zwracaja to co statement.execute()
             // Zajme sie zmienianiem tego wszystkiego, a Ty po prostu tego nie uzywaj, udawaj ze nie istnieje :D
-            return motorhomeDbRepository.create(motorhome);
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-        return false;
-    }
 
     //not sure if it will be needed
-    public List<Integer> getExistingMotorhomeDescriptionIds(){
+    public List<MotorhomeDescription> getExistingMotorhomeDescriptions (Motorhome motorhome){
         try{
-            return motorhomeDescriptionDbRepository.readAllIds();
+            motorhomeDescriptionDbRepository.readAll();
         }catch (SQLException e){
             e.printStackTrace();
         }
         return null;
+    }
+
+    public MotorhomeDescription addNewModel (String brand, String model, int capacity, int base_price){
+        MotorhomeDescription motorhomeDescription = new MotorhomeDescription();
+        motorhomeDescription.setBrand(brand);
+        motorhomeDescription.setModel(model);
+        motorhomeDescription.setCapacity(capacity);
+        motorhomeDescription.setBasePrice(base_price);
+        return motorhomeDescription;
     }
 /**
     public void addNewModel(MotorhomeDescription motorhomeDescription){
--- src/main/java/motorhomes/com/examproject/controller/MotorhomeController.java
+++ src/main/java/motorhomes/com/examproject/controller/MotorhomeController.java
@@ -2,19 +2,23 @@ package motorhomes.com.examproject.controller;
 
 import motorhomes.com.examproject.applicationLogic.MotorhomeManager;
 import motorhomes.com.examproject.model.Motorhome;
+import motorhomes.com.examproject.model.MotorhomeDescription;
 import motorhomes.com.examproject.repositories.MotorhomeDbRepository;
 import motorhomes.com.examproject.repositories.MotorhomeDescriptionDbRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * @ Alicja Drankowska
- * todo ????
+ * todo confirmPOST method and comment!
  */
 @Controller
 public class MotorhomeController {
@@ -26,20 +30,46 @@ public class MotorhomeController {
         this.motorhomeManager = new MotorhomeManager(new MotorhomeDbRepository(motorhomeDescriptionDbRepository), motorhomeDescriptionDbRepository);
     }
 
-    /**@GetMapping("/newmotorhome")
-    public String newmotorhome(){
-        return "newmotorhome";
-    }
-
-    @PostMapping("/newmotorhome")
-    public String newmotorhome(@RequestParam("description_id") int descriptionId, @ModelAttribute Motorhome motorhome){
-        motorhomeManager.createMotorhome(motorhome,descriptionId );
-        return "redirect:/fleet";
-    }
-     */
-
     @GetMapping("/fleet")
-    public String fleet(){
+    public String getAllMotorhomes(Model model){
+        List<Motorhome> motorhomes = motorhomeManager.getAllMotorhomes();
+        model.addAttribute("motorhomes", motorhomes);
         return"fleet";
     }
+
+    @GetMapping("/add_motorhome")
+    public String addNewMotorhome(){
+        return "add_motorhome";
+    }
+
+    @PostMapping("/add_motorhome")
+    public String addNewMotorhome(@RequestParam("licence_plate") String licencePlate, HttpServletRequest request, Model model){
+        Motorhome motorhome = motorhomeManager.addNewMotorhome(licencePlate);
+        request.getSession(true).setAttribute("motorhome", motorhome);
+        List<MotorhomeDescription> motorhomeDescriptions;
+        motorhomeDescriptions = motorhomeManager.getExistingMotorhomeDescriptions(motorhome);
+        model.addAttribute("motorhomeDescriptions", motorhomeDescriptions);
+        return "/create_description";
+    }
+
+    @PostMapping("/create_description")
+    public String addNewModel (HttpServletRequest request,
+        @RequestParam(value = "brand", required = false) String brand, @RequestParam(value = "model", required = false) String model,
+        @RequestParam(value = "capacity", required = false) int capacity, @RequestParam(value = "base_price", required = false) int base_price){
+
+        request.getSession().setAttribute("brand", brand);
+        request.getSession().setAttribute("model", model);
+        request.getSession().setAttribute("capacity", capacity);
+        request.getSession().setAttribute("base_price", base_price);
+        return "redirect:/confirm_newmotorhome";
+    }
+
+
+    @GetMapping("/save_motorhomeDescription")
+    public String saveMotorhomeDescription(@RequestParam("motorhomeDescription_id") int motorhomeDescriptionId, HttpServletRequest request){
+        Motorhome motorhome = (Motorhome)request.getSession(true).getAttribute("motorhome");
+        motorhomeManager.saveMotorhomeDescription(motorhome, motorhomeDescriptionId);
+        return "redirect:/confirm_newmotorhome";
+    }
+
 }
