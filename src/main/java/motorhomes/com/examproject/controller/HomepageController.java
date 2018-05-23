1783e3566309807caa1ecba9507dbe243b780fa5
Pawel Pohl <pawel.pohl229@gmail.com>
2018-05-24 00:55:13 +0200

    Login buh fixed.

--- src/main/java/motorhomes/com/examproject/controller/HomepageController.java
+++ src/main/java/motorhomes/com/examproject/controller/HomepageController.java
@@ -6,6 +6,7 @@ import motorhomes.com.examproject.repositories.UsersDBRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
